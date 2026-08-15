2553a7a8b7ca9f59fff77ee25c03cbf671e5601f
mcharef22 <mouadmami2003@gmail.com>
2026-08-15 22:23:31 +0200

    Temporarily disable email sending

--- jemos-back/controllers/email.controller.js
+++ jemos-back/controllers/email.controller.js
@@ -1,68 +1,13 @@
-const nodemailer = require("nodemailer");
-
-const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: process.env.SMTP_PORT,
-  secure: false,
-  auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASSWORD,
-  },
-});
-
 module.exports.sendEmail = async (req, res) => {
-  const { email, subject, message } = req.body;
-
-  const mailOptions = {
-    from: email,
-    to: process.env.SMTP_USER,
-    replyTo: email,
-    subject,
-    html: message,
-    attachments: [
-      {
-        filename: "jemos_logo.png",
-        path: "./imgs/jemos_logo.png",
-        cid: "logo",
-      },
-    ],
-  };
-
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.log(error);
-      res.status(500).send(error.message);
-    } else {
-      console.log("Email sent: " + info.response);
-      res.status(200).json(info.response);
-    }
+  console.log("SMTP désactivé temporairement");
+  return res.status(200).json({
+    message: "Email désactivé temporairement",
   });
 };
 
 module.exports.sendEmailToUser = async (req, res) => {
-  const { email, subject, message } = req.body;
-
-  const mailOptions = {
-    from: process.env.SMTP_USER,
-    to: email,
-    subject,
-    html: message,
-    attachments: [
-      {
-        filename: "jemos_logo.png",
-        path: "./imgs/jemos_logo.png",
-        cid: "logo",
-      },
-    ],
-  };
-
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.log(error);
-      res.status(500).send(error.message);
-    } else {
-      console.log("Email sent: " + info.response);
-      res.status(200).json(info.response);
-    }
+  console.log("SMTP désactivé temporairement");
+  return res.status(200).json({
+    message: "Email désactivé temporairement",
   });
 };
